8823e5b2a348f417dc6f60a4a37f9821a6a13b95
juan beresiarte <juanberesiarte@gmail.com>
2025-01-04 23:08:54 -0300

    Make config file

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const TITLE = "UI - CROBF";
+export const DESCRIPTION = "Descrubre una librería eficiente y sencilla.";
+export const KEYWORDS = ["Ui", "diseño", "CSS", "componente", "librería"];
